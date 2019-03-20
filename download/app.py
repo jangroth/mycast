25332dfd7d50e4d1b0fcfcf672f4e14d72a22574
Jan Groth <jan.groth.de@gmail.com>
2019-03-21 07:10:06 +1100

    Extract classes

--- download/app.py
+++ download/app.py
@@ -1,11 +1,37 @@
-import boto3
-import validators
+import logging
+
 import pytube
+import validators
 
 # Global variables are reused across execution contexts (if available)
-from pytube import YouTube
+# from pytube import YouTube
 
-session = boto3.Session()
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
+logging.basicConfig(
+    format='%(asctime)s %(name)-25s %(levelname)-8s %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger()
+logging.getLogger('boto3').setLevel(logging.ERROR)
+logging.getLogger('botocore').setLevel(logging.ERROR)
+
+
+class ProcessingError(Exception):
+    def __init__(self, http_return_code, message):
+        self.http_return_code = http_return_code
+        self.message = message
+
+
+def _get_url(event):
+    try:
+        url = event['body']['url']
+    except (KeyError, TypeError):
+        raise ProcessingError(400, '"url" is required in request body.')
+    if not validators.url(url):
+        raise ProcessingError(400, 'Invalid "url" in request body.')
+    return url
 
 
 def _create_response(status_code, text):
@@ -15,13 +41,29 @@ def _create_response(status_code, text):
     }
 
 
+def _get_bucket_name():
+    return 'TODO'
+
+
+class YoutubeToS3:
+    def __init__(self, url, bucket_name):
+        self.url = url
+        self.bucket_name = bucket_name
+
+    def run(self):
+        pass
+
+
 def lambda_handler(event, context):
+    logger.info('Entering handler')
     try:
-        url = event['body']['url']
-    except (KeyError, TypeError):
-        return _create_response(400, '"url" is required in request body.')
-    if not validators.url(url):
-        return _create_response(400, 'Invalid "url" in request body.')
-    yt = YouTube(url)
-    print(yt.streams.filter(only_audio=True).all())
-    return _create_response(200, 'Ok')
+        url = _get_url(event)
+        bucket_name = _get_bucket_name()
+    except ProcessingError as p:
+        logger.warning('Exiting with status {} - {}'.format(p.http_return_code, p.message))
+        return _create_response(p.http_return_code, p.message)
+
+    YoutubeToS3(url=url, bucket_name=bucket_name).run()
+    # yt = YouTube(url)
+    # print(yt.streams.filter(only_audio=True).all())
+    return _create_response(200, 'ok')
