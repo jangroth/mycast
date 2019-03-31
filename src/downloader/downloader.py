c8f9477d326b360f42a1c2dc5541d5c7fcd9a187
Jan Groth <jan.groth.de@gmail.com>
2019-03-31 16:29:34 +1100

    Add tests for downloader

--- src/downloader/downloader.py
+++ src/downloader/downloader.py
@@ -4,11 +4,10 @@ import os
 import boto3
 from pytube import YouTube
 
-root = logging.getLogger()
 logging.basicConfig(
     format='%(asctime)s %(name)-25s %(levelname)-8s %(message)s',
     level=logging.INFO)
-logger = logging.getLogger()
+log = logging.getLogger()
 logging.getLogger('boto3').setLevel(logging.ERROR)
 logging.getLogger('botocore').setLevel(logging.ERROR)
 
@@ -24,34 +23,34 @@ class Downloader:
         self._bucket = boto3.resource('s3').Bucket(bucket_name)
 
     def _get_youtube_object(self):
-        return YouTube(self.url)
+        youtube = YouTube(self.url)
+        youtube.register_on_progress_callback(self._on_progress)
+        return youtube
+
+    def _get_audio_stream(self, youtube):
+        stream = youtube.streams.filter(only_audio=True, mime_type="audio/mp4").order_by('abr').desc().first()
+        self._total_size = stream.filesize
+        return stream
 
     def _on_progress(self, stream, chunk, file_handle, bytes_remaining):
         percent_done = 100 - round(bytes_remaining / self._total_size * 100)
         if abs(percent_done - self._progress_percent) > 10:
-            logging.info('{}% done...'.format(percent_done))
+            log.info('{}% done...'.format(percent_done))
             self._progress_percent = percent_done
 
-    def _upload_to_s3(self, stream, file_handle):
-        logger.info('Download complete.')
-        file_name = os.path.basename(file_handle.name).replace(' ', '')
-        logger.info('Uploading "{}" into bucket "{}"'.format(file_name, self.bucket_name))
-        self._bucket.upload_file(file_handle.name, 'streams/{}'.format(file_name))
+    def _upload_to_s3(self, file_name):
+        log.info('Uploading "{}" into bucket "{}"'.format(file_name, self.bucket_name))
+        self._bucket.upload_file(file_name, 'streams/{}'.format(file_name))
 
     def _download_audio(self):
-        logging.info('Requesting download from {}...'.format(self.url))
-        youtube = YouTube(self.url)
-        youtube.register_on_progress_callback(self._on_progress)
-        youtube.register_on_complete_callback(self._upload_to_s3)
-        stream = youtube \
-            .streams \
-            .filter(only_audio=True, mime_type="audio/mp4") \
-            .order_by('abr') \
-            .desc() \
-            .first()
-        self._total_size = stream.filesize
-        logging.info('Starting download "{}"...'.format(youtube.title))
-        stream.download()
+        log.info('Requesting download from {}...'.format(self.url))
+        youtube = self._get_youtube_object()
+        stream = self._get_audio_stream(youtube)
+        log.info('Starting download of "{}"...'.format(youtube.title))
+        downloaded_file = stream.download()
+        log.info('Starting upload into bucket "{}"'.format(self.bucket_name))
+        self._upload_to_s3(downloaded_file)
+        log.info('Done')
 
     def run(self):
         self._download_audio()
--- tests/unit/test_downloader.py
+++ tests/unit/test_downloader.py
@@ -1,18 +1,21 @@
 import pytest
 
 from downloader.downloader import Downloader
-from receiver import app
 
 
 @pytest.fixture()
-def monkey_patched_downloader():
-    def noop():
-        print('noop invoked')
-
+def monkey_patched_downloader(mocker):
     dl = Downloader('foo', 'bar')
-    dl._get_youtube_object = noop
+    dl._get_youtube_object = mocker.MagicMock()
+    dl._get_audio_stream = mocker.MagicMock()
+    dl._upload_to_s3 = mocker.MagicMock()
+    dl._bucket = mocker.MagicMock()
     return dl
 
 
 def test_should_download(monkey_patched_downloader):
-    monkey_patched_downloader._get_youtube_object()
+    monkey_patched_downloader.run()
+
+    monkey_patched_downloader._get_youtube_object.assert_called_once()
+    monkey_patched_downloader._get_audio_stream.assert_called_once()
+    monkey_patched_downloader._upload_to_s3.assert_called_once()
